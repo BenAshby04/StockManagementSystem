dc81c9ff778eabb0cb9602f6e9c10d52b4f0bace
Ben Ashby <benashby@Bens-MacBook-Air.local>
2022-03-25 14:05:56 +0000

    Added a Delete function for Customer Profiles

--- main.py
+++ main.py
@@ -196,11 +196,14 @@ class manageCustomers():
         editCustomer.place(x=90,y=130, width=125,height=50)
         
         deleteCustomer = tk.Button(master=manageCustomerFrame,text="Delete a Customer")
+        deleteCustomer['command'] = self.deleteCustomer
         deleteCustomer.place(x=90,y=200,width=125,height=50)
     def addCustomer(self):
         addCustomers(self.win)
     def editCustomer(self):
         editCustomers(self.win)
+    def deleteCustomer(self):
+        deleteCustomers(self.win)
 
 class addCustomers():
     def __init__(self, previousWindow):
@@ -314,12 +317,13 @@ class editCustomers():
         if len(profiles) == 0:
             showinfo(title="Warning", message="There isn't any profiles with these details!")
         else:
-            SelectProfile(self.win, profiles)
+            SelectProfile(self.win, profiles, "edit")
 
 class SelectProfile():
-    def __init__(self,previousWindow, possibleProfiles):
+    def __init__(self,previousWindow, possibleProfiles, function):
         #Window Configuration
         self.profiles = possibleProfiles
+        self.function = function
         self.currentProfile = 0
         self.win = tk.Toplevel(previousWindow)
         self.win.title("Select a Profile")
@@ -435,7 +439,12 @@ class SelectProfile():
     def submit(self):
         #Goto edit values to make the changes to that profile.
         print(self.profiles[self.currentProfile])
-        editCustomerProfile(self.win, self.profiles[self.currentProfile])
+        if self.function == "edit":
+            editCustomerProfile(self.win, self.profiles[self.currentProfile])
+        elif self.function == "delete":
+            deleteProfile(self.win,self.profiles[self.currentProfile])
+        else:
+            showinfo("Error", "Error: class: SelectProfile, Function:submit, self.function is not 'edit' or 'delete'")
 
     
 class editCustomerProfile():
@@ -459,7 +468,7 @@ class editCustomerProfile():
         
         submitButton = tk.Button(master= editCustomerFrame, text="Submit")
         submitButton['command'] = self.commitToDB
-        submitButton.place(x=395,y=245,width=100,height=50 )
+        submitButton.place(x=395,y=245,width=100,height=50)
         
         #Label Configuration
         fNameLabel = tk.Label(master=editCustomerFrame, text="First Name:")
@@ -517,6 +526,131 @@ class editCustomerProfile():
         conn.close()
         print("Customer updated:")
         print("{0}, {1}, {2}, {3}".format(fName,lName,address,contact))
+        
+class deleteCustomers():
+    def __init__(self, previousWin):
+        #Window Configuration
+        self.win = tk.Toplevel(previousWin)
+        self.win.title("Delete a Customer")
+        self.win.geometry("500x300")
+        self.win.rowconfigure(0, weight=1)
+        self.win.columnconfigure(1, weight=1)
+        
+        #Frame Configure
+        deleteCustomerFrame = tk.Frame(master=self.win)
+        deleteCustomerFrame.grid(row=0 ,column=1, sticky="nsew")
+        
+        #Label Configuration
+        fNameLabel = tk.Label(master=deleteCustomerFrame, text="First Name:")
+        fNameLabel.place(x=115, y=60, width=100, height=20)
+        
+        lNameLabel = tk.Label(master=deleteCustomerFrame, text="Last Name:")
+        lNameLabel.place(x=115, y=90, width=100, height=20)
+        
+        #Button Configuration
+        exitButton = tk.Button(master=deleteCustomerFrame, text="Exit")
+        exitButton['command'] = self.win.destroy
+        exitButton.place(x=5,y=5, width=100,height=50)
+        
+        searchButton = tk.Button(master=deleteCustomerFrame, text="Search")
+        searchButton['command'] = self.findDataDB
+        searchButton.place(x=200,y=240, width=100,height=50)
+        
+        #Textbox Configuration
+        self.fNameText = tk.Text(master=deleteCustomerFrame)
+        self.fNameText.place(x=230, y=60, width=200,height=20)
+        
+        self.lNameText = tk.Text(master= deleteCustomerFrame)
+        self.lNameText.place(x=230,y=90,width=200,height=20)
+
+    def findDataDB(self):
+        fName = self.fNameText.get("1.0", "end").strip()
+        lName = self.lNameText.get("1.0", "end").strip()
+        
+        conn = sqlite3.connect("inventory.db")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM customer WHERE fName = '{0}' OR lName = '{1}'".format(fName, lName))
+        profiles = cur.fetchall()
+        print("Results:")
+        for profile in profiles:
+            print("{0},{1},{2},{3},{4}".format(profile[2],profile[3],profile[5],profile[4],profile[1]))
+        conn.close()
+        if len(profiles) == 0:
+            showinfo(title="Warning", message="There isn't any profiles with these details!")
+        else:
+            SelectProfile(self.win, profiles, "delete")
+                
+class deleteProfile():
+    def __init__(self, previousWin, profile):
+        #Window Configuration
+        self.profile = profile
+        self.win = tk.Toplevel(previousWin)
+        self.win.title("Delete This Profile?")
+        self.win.geometry("500x300")
+        self.win.rowconfigure(0, weight=1)
+        self.win.columnconfigure(1, weight=1)
+        
+        #Frame Configuration
+        deleteProfileFrame = tk.Frame(master = self.win)
+        deleteProfileFrame.grid(row=0, column=1, sticky="nsew")
+        
+        #Label Configuration
+        fNameLabel = tk.Label(master=deleteProfileFrame, text="First Name:")
+        fNameLabel.place(x=115,y=60, width=100,height=20)
+        
+        lNameLabel = tk.Label(master=deleteProfileFrame, text="Last Name:")
+        lNameLabel.place(x=115,y=90, width=100, height=20)
+        
+        contactLabel = tk.Label(master=deleteProfileFrame, text="Contact Number:")
+        contactLabel.place(x=91, y=120,width=110, height=20)
+        
+        addressLabel = tk.Label(master=deleteProfileFrame, text="Address:")
+        addressLabel.place(x=120, y=150, width=100,height=20)
+        
+        #Button Configuration
+        exitButton = tk.Button(master= deleteProfileFrame,text="Exit")
+        exitButton['command'] =self.win.destroy
+        exitButton.place(x=5,y=5,width=100, height=50)
+        
+        submitButton = tk.Button(master= deleteProfileFrame, text="Delete")
+        submitButton['command'] = self.deleteProfile
+        submitButton.place(x=395,y=245,width=100,height=50)
+        
+        #Textbox Configuration
+        self.fNameText = tk.Text(master=deleteProfileFrame)
+        self.fNameText.place(x=230,y=60,width=200,height=20)
+        
+        self.lNameText = tk.Text(master=deleteProfileFrame)
+        self.lNameText.place(x=230,y=90,width=200,height=20)
+        
+        self.contactText = tk.Text(master=deleteProfileFrame)
+        self.contactText.place(x=230,y=120,width=200,height=20)
+        
+        self.addressText = tk.Text(master=deleteProfileFrame)
+        self.addressText.place(x=230,y=150,width=200,height=20)
+        
+        self.loadProfile()
+        
+    def loadProfile(self):
+        fName = self.profile[2]
+        lName = self.profile[3]
+        address = self.profile[4]
+        contact = self.profile[5]
+        
+        self.fNameText.insert("1.0", fName)
+        self.lNameText.insert("1.0",lName)
+        self.addressText.insert("1.0",address)
+        self.contactText.insert("1.0",contact)
+        
+    def deleteProfile(self):
+        print("Deleting Profile: {0}".format(self.profile))
+        conn = sqlite3.connect("inventory.db")
+        cur = conn.cursor()
+        cur.execute("DELETE FROM customer WHERE CusID = '{0}'".format(self.profile[1]))
+        conn.commit()
+        conn.close()
+        
+        
 #POS Stuff
 class POSMenu():
     def __init__(self):
@@ -536,6 +670,8 @@ class POSMenu():
         exitButton['command'] = self.win.destroy
         exitButton.place(x=5,y=5,width=100,height=50)
 
+
+        
 
 def checkDatabaseExist():
     try:
